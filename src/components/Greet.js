907336e97c2acf469a3408a38b8ce5f87d330887
UmairAliShah <umairal43230@gmail.com>
2020-11-10 22:52:12 +0500

    -React package.json file is covered. -React Folder structure. -React components such function and class components are implemented.

---
+++ src/components/Greet.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// First Syntax of Stateless functional component
+
+/*
+function Greet() {
+    return (
+        <h1> Hi from first child component </h1>
+    );
+}
+*/
+
+// Second Syntax of Stateless functional component
+export const Greet = () => <h1> Hi from first child component! </h1>
+
+
+//export default Greet
---
+++ src/components/Welcome.js
@@ -0,0 +1,14 @@
+
+import React, { Component } from 'react';
+
+// Stateful Class Component
+class Welcome extends Component {
+  render() {
+    return (
+          <h1> Hello from Greeting Component </h1>
+    );
+  }  
+}
+
+
+export default Welcome
